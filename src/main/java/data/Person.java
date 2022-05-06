5e663d1ea2b133dc0108c86a379efc6dd37bbdb6
Virginia <virginia.ojeda9@gmail.com>
2022-05-06 17:21:36 +0200

    Initial commit

---
+++ src/main/java/data/Person.java
@@ -0,0 +1,14 @@
+package data;
+
+public class Person
+{
+    private String name;
+    public Person(String name)
+    {
+        this.name = name;
+    }
+    public String getName()
+    {
+        return name;
+    }
+}
---
+++ src/test/java/data/PersonTest.java
@@ -0,0 +1,16 @@
+package data;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest
+{
+    @Test
+    void getName()
+    {
+        Person p = new Person("James");
+        assertEquals("James", p.getName());
+    }
+}
